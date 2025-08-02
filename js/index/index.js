707e21710cfc8f4c934ccf9abc80d6f265770a07
Justin Berger <jusaleberger@gmail.com>
2025-08-02 15:04:27 -0600

    Modificación del Layout General del Sistema
    Se modifica los estilos del Sidebar y Top Navbar del Sistema, para que el sistema cumpla con un estándar de diseño. Se aplica este nuevo layout al Index.html y la Sección de Asistentes

---
+++ js/index/index.js
@@ -0,0 +1,4 @@
+// Toggle sidebar visibility on small screens
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    document.getElementById("wrapper").classList.toggle("toggled");
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,8 @@
-/*!
-    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2023 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
-// Scripts
-// 
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('sidebarToggle');
+  const wrapper = document.getElementById('wrapper');
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
-
+  toggleBtn.addEventListener('click', () => {
+    wrapper.classList.toggle('toggled');
+  });
 });
